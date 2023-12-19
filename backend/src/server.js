56b6eb86b5280a5f4521ddb5168ecf5b79c4d833
Lucas Cid <lucasfelipaaa@gmail.com>
2023-12-19 15:24:44 +0000

    changes at server.js to make userfull with developing errors, with express async errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const server = require('http')
 const cors = require('cors')
+const AppError = require('./utils/AppError')
 
 const app = express()
 app.use(cors())
@@ -10,7 +11,19 @@ const PORT =  process.env.PORT || 3333
 
 
 
+app.use((error, request, response, next) => {
+    if(error instanceof AppError){
+        return response.status(error.status).json({
+            message: error.message,
+            status : error.status
+        })
+    }
+    console.error(error)
+    return response.status(500).json({
+        message : "internal Server Error"
+    })
 
+})
 /**
  * server.createServer((req, resolve)=> {
     res.writeHead(200, {'Content-Type': 'text/plain'})
--- backend/src/services/userServices/UserRegisterService.js
+++ backend/src/services/userServices/UserRegisterService.js
@@ -7,11 +7,13 @@ class UserRegisterService{
     }
     
     execute = async ({username, email, password}) => {
+        //searching if this email is already in use
         const userExists = await this.userRepository.findByEmail(email)
+        
         if(userExists)throw new AppError('E-mail já em uso', 401)
 
         const hashedPassword = await hash(password, 8)
-
+        //if all is ok, register
         const newUser = await this.userRepository.signUp({username, password : hashedPassword, email})
 
         return newUser
